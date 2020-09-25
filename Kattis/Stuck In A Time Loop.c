1870eae7a6a43fb162861a37133796c35058362c
za <56107858+lrnzaa@users.noreply.github.com>
2020-09-26 06:11:00 +0700

    Create Stuck In A Time Loop.c

---
+++ Kattis/Stuck In A Time Loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+    int N, i;
+    
+    scanf("%d", &N);
+    
+    for(i = 0; i < N; i++)
+    {
+        printf("%d Abracadabra\n", i+1);
+    }
+    
+    return 0;
+}
